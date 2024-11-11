a4f829563385a0bd1bfa450479e270f5994a5d20
DevKodeAlx <145792803+DevKodeAlx@users.noreply.github.com>
2024-11-11 12:54:12 +0100

    Add files via upload

---
+++ js-sixth-task/script.js
@@ -0,0 +1,12 @@
+
+// Get a reference to the main section.
+
+const mainSection = document.querySelector('.maincontent');
+
+// Create a new text node with the desired text. 
+
+const textNode = document.createTextNode("Ad astra per aspera.");
+
+// Append the text node to the main section. 
+
+mainSection.appendChild(textNode);
